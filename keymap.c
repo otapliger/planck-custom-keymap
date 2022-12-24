dbf2083fdf0aa482436ddba5164b760a8547f52f
Otavio Pliger <37196762+otapliger@users.noreply.github.com>
2022-12-25 00:32:41 +0200

    Change arrows position

--- keymap.c
+++ keymap.c
@@ -73,13 +73,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
 │ Shift │   Z   │   X   │   C   │   V   │   B   │   N   │   M   │   ,   │   .   │   /   │ Enter │
 ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
-│ Ctrl  │  Alt  │ Super │       │ Raise │     Space     │ Lower │ Left  │ Down  │  Up   │ Right │
+│ Ctrl  │  Alt  │ Super │       │ Raise │     Space     │ Lower │  Up   │ Down  │ Left  │ Right │
 └───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────┘ */
 [_QWERTY] = LAYOUT_planck_mit(
     KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
     KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESFT,
-    KC_LCTL, KC_LOPT, KC_LCMD, XXXXXXX, RAISE, KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    KC_LCTL, KC_LOPT, KC_LCMD, XXXXXXX, RAISE, KC_SPC, LOWER, KC_UP, KC_DOWN, KC_LEFT, KC_RGHT
 ),
 
 /* Lower - FI
